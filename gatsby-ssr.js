4c0992a21dd263b9574b8d123e1f4d64fed75b8e
Dan Reeves <danreeves125@hotmail.com>
2022-03-01 16:50:11 +0000

    gatsby ssr updated

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,13 +1,14 @@
-import React from "react"
-
-import Layout from "./src/components/layout"
-import getRandomColor from './src/components/randomColourPicker'
-
+import getRandomColor from "./src/components/randomColourPicker";
+import Layout from "./src/components/layout";
+import React from "react";
 
 export const wrapPageElement = ({ element, props }) => {
+    let getColour = getRandomColor();
+
+    console.log(getColour.rgba);
 
     return (
-        <Layout {...props} color={getRandomColor()}>
+        <Layout {...props} color={getColour.hex} rgbaColour={getColour.rgba}>
             {element}
         </Layout>
     );
